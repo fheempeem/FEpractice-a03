f3047acc1a76295d475993f2cbfe4508d800de0c
fheempeem <peemplayz@gmail.com>
2026-03-11 15:20:14 +0700

    fix banner and change some name

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,7 +11,7 @@ export default function Banner () {
             objectFit="cover"/>
             <div className = {styles.bannerText}>
                 <h1>where every event finds its venue</h1>
-                <h3>Finding the perfect venue has never been easier. Whether it's a wedding, corporate event, or private party, we connecting people to ihe perfect place.</h3>
+                <h3>Finding the perfect venue has never been easier. Whether it's a wedding, corporate event, or private party, we connecting people to the perfect place.</h3>
             </div>
         </div>
     );
