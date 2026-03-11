735c9cbb18172cb678dad26dc20aa0ff875abec5
fheempeem <peemplayz@gmail.com>
2026-03-11 13:41:24 +0700

    initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+import styles from './card.module.css'
+import Image from 'next/image'
+
+export default function ProductCard() {
+    return (
+        <div className = {styles.card}>
+            <div className = {styles.cardimg}>
+                <Image src = {'/img/place1.jpg'}
+                alt = 'Place Picture'
+                fill = {true}
+                objectFit='cover'
+                />
+            </div>
+            <div className = {styles.cardtext}>
+                Machu Picchu
+                <div className = {styles.cardsubtext}>
+                This is a picture of Machu Picchuu.
+                </div>
+            </div>
+            
+        </div>
+    );
+}
